04413234d50cb52c1d7e002d5c277def328c5592
chilopod <xynwawj@126.com>
2018-03-12 13:16:16 +0800

    示例代码

---
+++ springdemo/src/main/java/isoftstone/springdemo/App.java
@@ -0,0 +1,13 @@
+package isoftstone.springdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
